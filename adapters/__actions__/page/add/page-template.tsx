87daf18cd22b6cbf6c34ab93ebf0a38f63536daa
Sébastien Vanson <sebastienvanson@hotmail.fr>
2024-03-17 11:36:46 +0100

    Updated beeker cli to 0.4.0

---
+++ adapters/__actions__/page/add/page-template.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@tremor/react';
+
+export default function ExamplePage() {
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <h1 className="text-center font-medium text-3xl">Title of the page</h1>
+      <Text>Subtitle of the page</Text>
+    </main>
+  );
+}
